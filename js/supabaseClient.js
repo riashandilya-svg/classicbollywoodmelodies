6407e08bb906f517dc15c57820d16c46effce016
riashandilya-svg <riashandilya@gmail.com>
2026-01-28 22:57:13 +0530

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,8 +1,6 @@
-<script type="module">
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-  const SUPABASE_URL = "https://lyqpxcilniqzurevetae.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5cXB4Y2lsbmlxenVyZXZldGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDAyMTQsImV4cCI6MjA4NTExNjIxNH0.40ZbAatkMBFHacQGCpiNYpjcKQoZik-Xvqx3bG46x7c";
+const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
+const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
 
-  window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
+window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
