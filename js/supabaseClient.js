3a7af6ca2861d5fe87c1490cbd4d8a08c47ebd71
riashandilya-svg <riashandilya@gmail.com>
2026-01-30 00:45:37 +0530

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -16,11 +16,10 @@
    const SUPABASE_URL = "https://lyqpxcilniqzurevetae.supabase.co";
   const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx5cXB4Y2lsbmlxenVyZXZldGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDAyMTQsImV4cCI6MjA4NTExNjIxNH0.40ZbAatkMBFHacQGCpiNYpjcKQoZik-Xvqx3bG46x7c";
 
-  const client = window.supabaseLib.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-  window.supabase = client;
-  window.supabase.supabaseKey = SUPABASE_ANON_KEY;
-  window.sb = client;
+ const client = window.supabaseLib.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = client;
+window.supabase.supabaseKey = SUPABASE_ANON_KEY;
+window.sb = client;
 
   console.log("Supabase client ready:", client);
 })();
